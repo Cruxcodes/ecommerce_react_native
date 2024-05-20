82d1b3b01866354ec5674e06b0411ccc67eca8c3
Crusader <abdullah.mudathir@isslng.com>
2024-05-20 13:12:23 +0100

    feat: first commit

---
+++ src/screens/splash/index.tsx
@@ -0,0 +1,16 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const SplashScreen = () => {
+  return (
+    <View>
+      <Text>This is the splash screen</Text>
+    </View>
+  );
+};
+
+export default SplashScreen;
+
+const styles = StyleSheet.create({});
+
+
